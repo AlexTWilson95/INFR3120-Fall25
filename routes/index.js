95cfb19306e77a5d5db92b1a8dd241befa6fbe30
AlexTWilson95 <Alexander.wilson@ontariotechu.net>
2025-11-13 14:46:32 -0500

    updating index routes for pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Ontario Tech Casino' });
 });
 
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/register', (req, res) => {
+  res.render('register', { title: 'Register' });
+});
+
+router.get('/feature', (req, res) => {
+  res.render('feature', { title: 'OnTech Casino Game Menu', wallet: 0  /* placeholder until MongoDB is connected */ });
+});
+
+router.get('/deposit', (req, res) => {
+  res.render('deposit', { title: 'Deposit Funds' });
+});
+
+router.get("/withdraw", (req, res) => {
+    res.render("withdraw", { title: 'Withdraw Winnings'});
+});
+
+
 module.exports = router;
+
